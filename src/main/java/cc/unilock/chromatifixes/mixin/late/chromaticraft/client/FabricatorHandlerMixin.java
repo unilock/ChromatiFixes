7b43bfe55b8054d24da9c84e8eabd046af91809f
unilock <unilock@fennet.rentals>
2025-05-19 05:45:42 -0400

    fix ChromatiCraft's FabricatorHandler#getGuiClass
    thanks again @FlamingKetchup!
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/chromatifixes/mixin/late/chromaticraft/client/FabricatorHandlerMixin.java
+++ src/main/java/cc/unilock/chromatifixes/mixin/late/chromaticraft/client/FabricatorHandlerMixin.java
@@ -1,14 +1,31 @@
 package cc.unilock.chromatifixes.mixin.late.chromaticraft.client;
 
+import Reika.ChromatiCraft.GUI.Tile.Inventory.GuiItemFabricator;
 import Reika.ChromatiCraft.ModInterface.NEI.FabricatorHandler;
 import codechicken.nei.recipe.TemplateRecipeHandler;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
 
 import java.awt.*;
 
 @Mixin(value = FabricatorHandler.class, remap = false)
 public abstract class FabricatorHandlerMixin extends TemplateRecipeHandler {
+    /**
+     * @author FlamingKetchup
+     * @reason Allow opening NEI from Item Fabricator UI
+     */
+    @ModifyReturnValue(method = "getGuiClass", at = @At("RETURN"))
+    private Class<? extends GuiContainer> getGuiClass(Class<? extends GuiContainer> original) {
+        if (original == null) {
+            return GuiItemFabricator.class;
+        } else {
+            return original;
+        }
+    }
+
     /**
      * @author FlamingKetchup
      * @reason Remove client player openContainer instanceof check; @Overwrite to fail hard
